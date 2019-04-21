1f1a13e52b61adaba3bd9d5413433fe43070aacc
Caleb Aviles <earthctzn1@gmail.com>
2019-04-21 18:49:29 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.start_with?(/\b[aeiou AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(words)
+  words.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
+  if text.match(/^[A-Z].*\.$/) == nil
+        return false 
+    else
+        return true  
+    end
+  end
+  
+def valid_phone_number?(number)
+  if number.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
