671e3789983caf958237fe93704e0fe883c7a2a8
zhenggao cai <caizhengao1992@gmail.com>
2019-04-29 03:08:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
-def starts_with_a_vowel?(word)
 
-end
+def starts_with_a_vowel?(word)
+  word.match(/\A[aeiouAEIOU]/) ? true : false
+end 
+  
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+    if word.match(/(^un)|(ing$)/)
+      text_array.push(word)
+    end
+  end
+  return text_array
 end
 
 def words_five_letters_long(text)
-
+  text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+  if word.match(/^[a-zA-Z]{5}$/)
+    text_array.push(word)
+  end
+end
+return text_array
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) && text.match(/[ _.,!"']$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/) ? true : false
 end
